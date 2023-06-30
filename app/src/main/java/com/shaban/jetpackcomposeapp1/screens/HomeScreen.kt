67859ced00c6ce25f80ee4c0a4f26ecb260d751b
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-06-30 04:25:47 +0300

    implement row

--- app/src/main/java/com/shaban/jetpackcomposeapp1/screens/HomeScreen.kt
+++ app/src/main/java/com/shaban/jetpackcomposeapp1/screens/HomeScreen.kt
@@ -2,6 +2,7 @@ package com.shaban.jetpackcomposeapp1.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -10,7 +11,7 @@ import com.shaban.jetpackcomposeapp1.composable.HelloMessage
 
 @Composable
 fun HomeScreen(modifier: Modifier) {
-    Column(
+    /*Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceAround
@@ -18,5 +19,14 @@ fun HomeScreen(modifier: Modifier) {
         HelloMessage(name = "Mohamed", modifier = modifier)
         HelloMessage(name = "Shaban", modifier = modifier)
         HelloMessage(name = "Ahmed", modifier = modifier)
+    }*/
+
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        HelloMessage(name = "Mohamed", modifier = modifier)
+        HelloMessage(name = "Shaban", modifier = modifier)
     }
 }
