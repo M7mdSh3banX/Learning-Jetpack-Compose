c9d78135377436ccd2dc48d4619eb280661c99d6
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-06-30 05:40:31 +0300

    implement size with width and height and font size

--- app/src/main/java/com/shaban/jetpackcomposeapp1/screens/HomeScreen.kt
+++ app/src/main/java/com/shaban/jetpackcomposeapp1/screens/HomeScreen.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,6 +15,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen(modifier: Modifier) {
@@ -24,9 +28,18 @@ fun HomeScreen(modifier: Modifier) {
     ) {
         Text(
             modifier = Modifier
-                .fillMaxWidth()
-                .background(color = Color.Cyan),
-            text = "Hello Mohamed"
+                .background(color = Color.Gray)
+                .width(200.dp)
+                .height(100.dp),
+            text = "Hello Mohamed",
+            fontSize = 24.sp
+        )
+        Text(
+            modifier = Modifier
+                .background(color = Color.Gray)
+                .size(width = 96.dp, height = 128.dp),
+            text = "Hello Shaban",
+            fontSize = 20.sp
         )
         Button(onClick = { /*TODO*/ }) {
             Text(text = "Click Me")
