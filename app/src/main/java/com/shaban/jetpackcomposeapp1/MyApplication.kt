33b46221ba79fc608bd231a51ec2bfb758e136ea
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-07-01 03:01:25 +0300

    implement resources

--- app/src/main/java/com/shaban/jetpackcomposeapp1/MyApplication.kt
+++ app/src/main/java/com/shaban/jetpackcomposeapp1/MyApplication.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import com.shaban.jetpackcomposeapp1.screens.HomeScreen
 import com.shaban.jetpackcomposeapp1.ui.theme.JetpackComposeApp1Theme
 
@@ -25,7 +26,7 @@ fun MyApp() {
         Scaffold(
             topBar = {
                 TopAppBar(
-                    title = { Text(text = "My Application") },
+                    title = { Text(text = stringResource(id = R.string.app_name)) },
                     // colors = TopAppBarDefaults.mediumTopAppBarColors(Color.Green)
                 )
             },
--- app/src/main/java/com/shaban/jetpackcomposeapp1/screens/HomeScreen.kt
+++ app/src/main/java/com/shaban/jetpackcomposeapp1/screens/HomeScreen.kt
@@ -1,16 +1,14 @@
 package com.shaban.jetpackcomposeapp1.screens
 
-import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,16 +16,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.shaban.jetpackcomposeapp1.modifiers.blueLight
+import com.shaban.jetpackcomposeapp1.R
+import com.shaban.jetpackcomposeapp1.composable.GlowText
 
 @Composable
 fun HomeScreen(modifier: Modifier) {
     val textShadowModifier = Modifier
         .shadow(elevation = 12.dp, spotColor = Color.Red)
-        .background(color = Color.Red)
+        .background(color = Color.LightGray)
         .fillMaxWidth()
         .padding(8.dp)
 
@@ -43,26 +44,15 @@ fun HomeScreen(modifier: Modifier) {
                 fontSize = 20.sp
             )
         }
-        Text(
-            modifier = Modifier
-                .blueLight(),
-            text = "Hello Shaban",
-            fontSize = 20.sp
+        Image(
+            painter =
+            painterResource(id = R.drawable.clarence),
+            contentDescription = null,
+            modifier = Modifier.size(256.dp)
         )
-        Text(
-            modifier = Modifier
-                .clickable {
-                    Log.e("CLICKABLE", "Hello I'm Clickable")
-                }
-                .background(color = Color.Red)
-                .width(200.dp)
-                .height(100.dp)
-                .padding(16.dp)
-                .background(color = Color.Green)
-                .padding(16.dp)
-                .background(color = Color.Yellow),
-            text = "Hello Mohamed",
-            fontSize = 24.sp
+        GlowText(
+            name = "Mohamed",
+            modifier = Modifier.background(colorResource(id = R.color.purple_200))
         )
         Button(
             modifier = Modifier.fillMaxWidth(),
